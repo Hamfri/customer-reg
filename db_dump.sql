e446a36e1c10edb47fc283ae00d019f1e87a37b9
Humphrey Ndirangu <humphrey.ndirangu@appsolute-mobility.com>
2021-02-23 16:33:26 +0100

    server: add database dump

--- db_dump.sql
+++ db_dump.sql
@@ -32,7 +32,7 @@ CREATE TABLE `bank_accounts` (
   PRIMARY KEY (`id`),
   KEY `bank_accounts_customer_id_foreign` (`customer_id`),
   CONSTRAINT `bank_accounts_customer_id_foreign` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `bank_accounts` (
 
 LOCK TABLES `bank_accounts` WRITE;
 /*!40000 ALTER TABLE `bank_accounts` DISABLE KEYS */;
-INSERT INTO `bank_accounts` VALUES (1,1,'Humphrey Ndirangu','DE75512108001245126211','2021-02-19 21:14:46','2021-02-19 21:14:46'),(2,2,'Joshua Mukua','DE75512108001245126212','2021-02-19 21:15:46','2021-02-19 21:15:46'),(3,4,'Joshua Mukua','DE75512108001245126214','2021-02-20 22:03:22','2021-02-20 22:03:22'),(4,5,'Ham tester','72555f5f5f5f','2021-02-20 22:20:22','2021-02-20 22:20:22'),(5,6,'pppppp','DE364562888889','2021-02-21 14:26:56','2021-02-21 14:26:56'),(6,8,'pppppp','DE3645628888899','2021-02-21 14:55:54','2021-02-21 14:55:54'),(7,9,'Humphrey Kiboi','DE369852222111','2021-02-21 15:01:14','2021-02-21 15:01:14'),(8,10,'Zachariah Obiek','DE369896535555222','2021-02-21 17:22:38','2021-02-21 17:22:38'),(9,11,'pop','De895622','2021-02-21 18:29:51','2021-02-21 18:29:51');
+INSERT INTO `bank_accounts` VALUES (1,1,'Test user 1','DE3895','2021-02-23 15:24:30','2021-02-23 15:24:30'),(2,2,'Tester 36','DE7859236555','2021-02-23 15:25:44','2021-02-23 15:25:44');
 /*!40000 ALTER TABLE `bank_accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +65,7 @@ CREATE TABLE `customers` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `customers_telephone_unique` (`telephone`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +74,7 @@ CREATE TABLE `customers` (
 
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers` DISABLE KEYS */;
-INSERT INTO `customers` VALUES (1,'Humphrey','Ndirangu','015255111022','Clara Zetkin str. 1','WE07 G Links',99094,'Erfurt','2021-02-19 21:14:46','2021-02-19 21:14:46'),(2,'Humphrey','Ndirangu','015255111023','Blumenschmidt str. 1','WE07 G Links',99084,'Erfurt','2021-02-19 21:15:46','2021-02-19 21:15:46'),(4,'Humphrey','Ndirangu','015255111027','Blumenschmidt str. 1','WE07 G Links',99084,'Erfurt','2021-02-20 22:03:21','2021-02-20 22:03:21'),(5,'Humphrey','Ndirangu','0152558605858','Blumenschmidtstraße 1','we0589',99085,'Erfurt','2021-02-20 22:20:21','2021-02-20 22:20:21'),(6,'Humphrey','Ndirangu','0152558605856','Blumenschmidtstraße 1','we07',99085,'Erfurt','2021-02-21 14:26:54','2021-02-21 14:26:54'),(8,'Humphrey','Ndirangu','01525586058565','Blumenschmidtstraße 1','we07',99085,'Erfurt','2021-02-21 14:55:54','2021-02-21 14:55:54'),(9,'Humphrey','Ndirangu','01525586058578','Blumenschmidtstraße 1','we05',99085,'Erfurt','2021-02-21 15:01:14','2021-02-21 15:01:14'),(10,'Zachariah','Obiek','0711676822','Robert Koch','FE07 Links',25639,'Hamburg','2021-02-21 17:22:38','2021-02-21 17:22:38'),(11,'Humphrey','Ndirangu','01255860585','Blumenschmidtstraße 1','WE09',99085,'Erfurt','2021-02-21 18:29:50','2021-02-21 18:29:50');
+INSERT INTO `customers` VALUES (1,'Humphrey','Ndirangu','017255860587','Blumenschmidtstraße 1','we078',99056,'Erfurt','2021-02-23 15:24:30','2021-02-23 15:24:30'),(2,'Joshua','Swagui','017562318822','Blumenschmidtstraße 1','we09',99085,'Erfurt','2021-02-23 15:25:44','2021-02-23 15:25:44');
 /*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -119,7 +119,7 @@ CREATE TABLE `transactions` (
   PRIMARY KEY (`id`),
   KEY `transactions_customer_id_foreign` (`customer_id`),
   CONSTRAINT `transactions_customer_id_foreign` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +128,7 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-INSERT INTO `transactions` VALUES (1,1,'2bf8fba60f713d698022be772293a35c40af86c7d20ceed202633c79c493b47d311e4cea792ad0af1b73f1757b76f870','2021-02-19 21:14:46','2021-02-19 21:14:46'),(2,2,'0a1367d6b1c0c9e41336502fa07b3f0c66c33d44bcffad0e899cb7f25469824f099cb1f2fcc1c00d91483820c8e3ba24','2021-02-19 21:15:47','2021-02-19 21:15:47'),(3,4,'925a224c1e19c80f14e78b2a312fd37d790d211bfa9a54edafc24828d2bbc892d2b05579e76d2edae9a79fb4b8456e93','2021-02-20 22:03:24','2021-02-20 22:03:24'),(4,5,'7371fac3d01d407a20a255ead1f7d25262c4f5de14f52378b39c82eab1971c2ac3dbfec8aa988c2ef533dfd623237343','2021-02-20 22:20:22','2021-02-20 22:20:22'),(5,6,'270ccbf7019cfbaa73df063dcd1000d072633537fcf94ef9a94a67c0694710bdc057abdccc7eb18d270745a69a30cb55','2021-02-21 14:26:58','2021-02-21 14:26:58'),(6,8,'24bfdbdf96cf3c35e2368d2da149d6d45d495bd18e34041eaf34d6b1ee55993d25f348f77f6a64bb1c6286f5466100a3','2021-02-21 14:55:55','2021-02-21 14:55:55'),(7,9,'6e482ba0651ecdbba8ef79301f04bb3f40ce87fdd7c6a027c0e2a182297b134fedcdf62fc46316f6b76d73890c0d630c','2021-02-21 15:01:15','2021-02-21 15:01:15'),(8,10,'86129a252655e181e7c4f66579868712b680f6cbabebec6e417b4a3605f687ce056458836c1ed34076dd70ccc90d9a7f','2021-02-21 17:22:39','2021-02-21 17:22:39'),(9,11,'6a52e21a6e712f6323a04943b856c2bcb54faeb7522b0a9f023a5a5e19020bea6c8193496fd6e13a0966ce575d65cbef','2021-02-21 18:29:52','2021-02-21 18:29:52');
+INSERT INTO `transactions` VALUES (1,1,'f034aeea509c1a7ce6bda9532948c51266527bb41b4f4855abbdad59a990e42c6747f74ad0fa4c660e1e1ecdb3a054ea','2021-02-23 15:24:31','2021-02-23 15:24:31'),(2,2,'3eb398caddcdfc73bf26a0651fcbc5dbcedc1350064ac1dd54d44ba898ad3b65c7da354578024aac2917d441cc06dbe3','2021-02-23 15:25:44','2021-02-23 15:25:44');
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -141,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-21 21:19:54
+-- Dump completed on 2021-02-23 15:29:42
