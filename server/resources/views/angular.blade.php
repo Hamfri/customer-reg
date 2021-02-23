3949a7c490fd56304d8026783646e6f13b9ddff4
Humphrey Ndirangu <humphrey.ndirangu@appsolute-mobility.com>
2021-02-23 16:01:26 +0100

    server: regenerate assets

--- server/resources/views/angular.blade.php
+++ server/resources/views/angular.blade.php
@@ -7,10 +7,10 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="/assets/angular/styles.21db3fe62a4c1ef48fd2.css"></head>
+<link rel="stylesheet" href="/assets/angular/styles.da737bdc07d0813790e3.css"></head>
 
 <body>
   <app-root></app-root>
-<script src="/assets/angular/runtime.96254822893afa4fd974.js" defer></script><script src="/assets/angular/polyfills.8c151b8375e767ff858f.js" defer></script><script src="/assets/angular/scripts.cca327114593ed4ead26.js" defer></script><script src="/assets/angular/main.be31005720d28f50e40d.js" defer></script></body>
+<script src="/assets/angular/runtime.96254822893afa4fd974.js" defer></script><script src="/assets/angular/polyfills.8c151b8375e767ff858f.js" defer></script><script src="/assets/angular/scripts.cca327114593ed4ead26.js" defer></script><script src="/assets/angular/main.f5af642a63943e3ded7e.js" defer></script></body>
 
 </html>
